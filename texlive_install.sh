e6a34f12e75b8d367b7d299d749802c8b53c8074
Thomas <hollandpirates@gmail.com>
2017-10-30 20:56:12 +0100

    try to get xkeyval.sty

--- texlive_install.sh
+++ texlive_install.sh
@@ -25,10 +25,11 @@ fi
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
 
+tlmgr install xkeyval
+
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
-  collection-latex \
   exam          \
   amsfonts      \
   amsmath       \
